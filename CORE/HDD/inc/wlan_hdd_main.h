c508875c0298bb8baaf15063e0b048f4665b2cc3
SaidiReddy Yenuga <c_saidir@qti.qualcomm.com>
2016-10-26 15:54:47 +0530

    wlan: Fix compilation error
    Compilation error happening with WLAN_FEATURE_RMC disabled.
    
    Remove ifdef WLAN_FEATURE_RMC macro in wlan_hdd_main.h.
    
    CRs-Fixed: 1082451
    Change-Id: I390c65ffc8fb534c801831f879053eb9664f0738

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -672,7 +672,6 @@ typedef enum{
     HDD_SSR_DISABLED,
 }e_hdd_ssr_required;
 
-#ifdef WLAN_FEATURE_RMC
 /*---------------------------------------------------------------------------
   hdd_ibss_peer_info_params_t
 ---------------------------------------------------------------------------*/
@@ -731,7 +730,6 @@ typedef struct
     /** Peer Info parameters */
     hdd_ibss_peer_info_params_t  ibssPeerList[HDD_MAX_NUM_IBSS_STA];
 }hdd_ibss_peer_info_t;
-#endif
 
 struct hdd_station_ctx
 {
@@ -760,9 +758,7 @@ struct hdd_station_ctx
 
    /*Save the wep/wpa-none keys*/
    tCsrRoamSetKey ibss_enc_key;
-#ifdef WLAN_FEATURE_RMC
    hdd_ibss_peer_info_t ibss_peer_info;
-#endif
 
    v_BOOL_t hdd_ReassocScenario;
 
