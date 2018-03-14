82d48b44e034ba755e37b28d826f36d2c88322d7
Hanumanth Reddy Pothula <c_hpothu@codeaurora.org>
2018-03-14 14:32:27 +0530

    wlan: Introduce HDD Request Manager infrastructure
    qcacld-3.0 to prima propagation.
    
    Many operations within the wlan driver occur in an asynchronous
    manner. Requests are received by HDD via one of the kernel interfaces
    (ioctl, nl80211, virtual file system, etc.). The requests are
    translated to an internal format and are then passed to lower layers
    for processing. For requests which require a response, that response
    comes up from the lower layers in a separate thread of execution,
    ultimately resulting in a call to a callback function that was
    provided by HDD as part of the initial request. So a mechanism is
    needed to synchronize the request and response.
    
    Currently there are various mechanisms which perform these
    synchronizations, but experience with them has revealed some flaws. So
    a universal mechanism is needed to synchronize the request and
    response which addresses all of the known flaws. This framework
    provides that mechanism.
    
    Change-Id: If4d5912710f8a3b5e87adf76f828a646b7cc2983
    CRs-Fixed: 2205457

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -123,6 +123,7 @@ int wlan_hdd_ftm_start(hdd_context_t *pAdapter);
 #ifdef CONFIG_HUAWEI_WIFI_BUILTIN
 #include <linux/types.h>
 #endif
+#include "wlan_hdd_request_manager.h"
 
 #ifdef MODULE
 #define WLAN_MODULE_NAME  module_name(THIS_MODULE)
@@ -11995,6 +11996,9 @@ free_hdd_ctx:
        kfree(pHddCtx->cfg_ini);
        pHddCtx->cfg_ini= NULL;
    }
+
+   hdd_request_manager_deinit();
+
    /* FTM/MONITOR mode, WIPHY did not registered
       If un-register here, system crash will happen */
    if (!(VOS_FTM_MODE == hdd_get_conparam() ||
@@ -13178,6 +13182,8 @@ int hdd_wlan_startup(struct device *dev )
       goto err_free_hdd_context;
    }
 
+   hdd_request_manager_init();
+
    vos_mem_zero(pHddCtx->cfg_ini, sizeof( hdd_config_t ));
 
    // Read and parse the qcom_cfg.ini file
@@ -14059,6 +14065,7 @@ err_wdclose:
       vos_watchdog_close(pVosContext);
 
 err_config:
+   hdd_request_manager_deinit();
    kfree(pHddCtx->cfg_ini);
    pHddCtx->cfg_ini= NULL;
 
